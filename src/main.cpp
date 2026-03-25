01521282e2d2f3126fe2e48321818508fbba29c0
oscar <oscar.dujardin44@gmail.com>
2026-03-25 18:10:08 +0100

    pour qu'il n'y ait pas de pb de format d'encodage

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
+#include <iostream>
+#include <windows.h>
 #include "Game.h"
 
 int main() {
+    // Cette ligne dit à Windows : "Oublie le vieux format, utilise l'UTF-8"
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8); // Optionnel : pour que l'entrée clavier soit aussi en UTF-8
+
     Game game;
     game.run();
     return 0;
